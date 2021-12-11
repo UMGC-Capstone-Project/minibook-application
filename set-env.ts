a59a191f4b948a10a81c2f26c609f5c0ab024ea8
David Mydlarz <coldfront@gmail.com>
2021-12-11 09:40:38 +0900

    wrap strings for env vars

--- set-env.ts
+++ set-env.ts
@@ -2,10 +2,10 @@ const fs = require('fs');
 // import { writeFile } from 'fs'; if you are using a typescript file
 
 const environmentFile = `export const environment = {
-  imgUrl: ${process.env.IMG_URI},
-  apiUrl: ${process.env.API_URI},
-  baseUrl: ${process.env.BASE_URI},
-  production: '${process.env.PRODUCTION}'
+  imgUrl: '${process.env.IMG_URI}',
+  apiUrl: '${process.env.API_URI}',
+  baseUrl: '${process.env.BASE_URI}',
+  production: ${process.env.PRODUCTION}
 };
 `;
 
